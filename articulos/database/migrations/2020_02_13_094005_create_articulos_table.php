e9e4235a16ad8bbd84987dec94cf1581903bbb72
Javier <svjaviergarcia@gmail.com>
2020-02-13 10:50:55 +0100

    Parte 1
    Parte 1

---
+++ articulos/database/migrations/2020_02_13_094005_create_articulos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticulosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articulos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre')->unique();
+            $table->string('categoria');
+            $table->float('precio',3,2);
+            $table->string('stock');
+            $table->string('foto')->default('/img/articulos/default.jpg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articulos');
+    }
+}
